2b71f75d92c887b4ae8ce715e1393fdd4b3ad09a
nicolaslee2000 <nicolaslee2000@gmail.com>
2022-03-24 17:48:23 +0900

    day2 final commit

---
+++ javademo/src/java0323/Prob01.java
@@ -0,0 +1,24 @@
+package java0323;
+/*
+ * return 1 if num is factor of 10
+ */
+public class Prob01 {
+	static int res;
+	public static void main(String[] args) {
+		
+		factorOfTen(21);
+		System.out.println(returnAbsoluteValue(-22));
+	}
+	
+	public static void factorOfTen(int x) {
+		
+		res = x%10 == 0 ? 1 : 0;
+		System.out.println(res);
+	}
+	
+	public static int returnAbsoluteValue(int x) {
+		System.out.println(Math.abs(x));
+		return x<0 ? x*-1 : x;
+		
+	}
+}
