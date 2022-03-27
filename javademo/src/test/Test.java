360a607a445be0d1c765662b2d4eda7768864e13
nicolaslee2000 <nicolaslee2000@gmail.com>
2022-03-27 22:46:08 +0900

    example code of stackoverflow Test02class

--- javademo/src/test/Test.java
+++ javademo/src/test/Test.java
@@ -1,59 +1,35 @@
 package test;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.nio.ByteBuffer;
-import java.nio.charset.CharacterCodingException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.HashSet;
+
+class TestClass {
+	HashSet<File> listOfFiles = new HashSet<File>();
+	
+	//returns all files in the directory as a set
+	static HashSet<File> getAllFiles(File file) {
+		HashSet<File> listOfFiles = new HashSet<File>();
+		if(file.isDirectory()) {
+			for(File subFiles : file.listFiles()) {
+				if(subFiles.isDirectory()) {
+					getAllFiles(subFiles);
+				} else {
+					listOfFiles.add(subFiles);				
+					}
+			}
+		} else {
+			listOfFiles.add(file);				
+			}
+//		listOfFiles.clear();
+		return listOfFiles;
+	}
+}
 
 public class Test {
 	public static void main(String[] args) {
-		try (BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File("C:\\Users\\nicol\\Documents\\converting with java\\Test01.txt")));){
-			int a = 0;
-			identified:
-			while (true) {
-				System.out.println(a*10);
-				for(int i = 0; i<10;i++) {
-					
-					try {
-						if(i == 3) {
-							System.out.println(i);
-						}
-						else {
-							System.out.println(i/0);
-						}
-						
-					} catch (Exception e) {
-						System.out.println("error");
-						continue;
-					}
-					System.out.println("breaking");
-					break identified;
-				}
-				a++;
-	        }
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} 
+		System.out.println(TestClass.getAllFiles(new File("C:\\Users\\nicol\\Documents\\converting with java\\Testfolder")));
+		System.out.println(TestClass.getAllFiles(new File("C:\\Users\\nicol\\Documents\\converting with java\\Testfolder\\javalec\\basic")));
+		
 		
 	}
 }
---
+++ javademo/src/test/Test02.java
@@ -0,0 +1,34 @@
+package test;
+
+
+import java.io.File;
+import java.util.HashSet;
+
+class TestClass2 {
+	HashSet<File> listOfFiles = new HashSet<File>();
+	
+	//returns all files in the directory as a set
+	static HashSet<File> getAllFiles(File file) {
+	    HashSet<File> set = new HashSet<>();
+	    File[] files = file.listFiles();
+	    if (files != null) {
+	        for (File f : files) {
+	            if (f.isDirectory()) {
+	                set.addAll(getAllFiles(f));
+	            } else {
+	                set.add(f);
+	            }
+	        }
+	    }
+	    return set;
+	}
+}
+
+public class Test02 {
+	public static void main(String[] args) {
+		System.out.println(TestClass2.getAllFiles(new File("C:\\Users\\nicol\\Documents\\converting with java\\Testfolder")));
+		System.out.println(TestClass2.getAllFiles(new File("C:\\Users\\nicol\\Documents\\converting with java\\Testfolder\\javalec\\basic")));
+		
+		
+	}
+}
