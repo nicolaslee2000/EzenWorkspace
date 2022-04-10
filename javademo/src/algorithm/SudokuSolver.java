a50612948a705a70709e7fab348e061adac9ac40
nicolaslee2000 <nicolaslee2000@gmail.com>
2022-04-11 07:51:31 +0900

    sudokusovler solved

--- javademo/src/algorithm/SudokuSolver.java
+++ javademo/src/algorithm/SudokuSolver.java
@@ -1,189 +1,242 @@
-package algorithm;
-
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-
-
-public class SudokuSolver {
-	public static void main(String[] args) {
-		Solver solver = new Solver();
-		
-		
-	}
-}
-
-class Solver extends JFrame{
-	JPanel panel;
-	JPanel panel1;
-	JButton btnSet;
-	JButton btnSolve;
-	JButton btnReset;
-	JTextField[][] textfields;
-	int[][] board;
-	int[] num;
-	
-	Solver() {
-		
-		setNum();
-		initBoard();
-		this.pack();
-		this.setSize(420,420);
-		this.setLocationRelativeTo(null);
-		this.setVisible(true);
-		setBtns();
-	}
-	
-	private void initBoard () {
-		panel = new JPanel();
-		panel.setLayout(new GridLayout(9, 9));
-		textfields = new JTextField[9][9]; 
-		for(int i = 0; i < 9; i++) {
-			for(int j = 0; j < 9; j++) {
-				textfields[i][j] = new JTextField();
-				textfields[i][j].setFont(new Font("Tahoma", Font.BOLD, 17));
-				textfields[i][j].setHorizontalAlignment(JTextField.CENTER);
-//				textfields[i].setText(String.valueOf(i));
-				panel.add(textfields[i][j]);
-				textfields[i][j].getDocument().addDocumentListener(new Doclistener(i,j));
-			}
-		}
-		getContentPane().add(panel, BorderLayout.CENTER);
-		panel1 = new JPanel();
-		btnSet = new JButton("Set");
-		panel1.add(btnSet);
-		btnSolve = new JButton("Solve");
-		panel1.add(btnSolve);
-		btnReset = new JButton("Reset");
-		panel1.add(btnReset);
-		getContentPane().add(panel1, BorderLayout.SOUTH);
-	}
-	
-	class Doclistener implements DocumentListener {
-		int i;
-		int j;
-		Doclistener(int i, int j) {
-			this.i = i;
-			this.j = j;
-		}
-		@Override
-		public void insertUpdate(DocumentEvent e) {
-			update(e);
-		}
-		@Override
-		public void removeUpdate(DocumentEvent e) {
-			update(e);
-		}
-		@Override
-		public void changedUpdate(DocumentEvent e) {
-			update(e);
-		}
-		void update(DocumentEvent e) {
-			if(e.getDocument().getLength()>0){
-				if(j==8) {
-					if(i==8) return;
-					textfields[i+1][0].requestFocus();
-					return;
-				} 
-				textfields[i][j+1].requestFocus();
-			}
-		}
-	}
-	
-	private void setBtns() {
-		btnSet.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				for(int i = 0; i < 9; i++) {
-					for(int j = 0; j < 9; j++) {
-						if(!textfields[i][j].getText().isBlank()) {
-							textfields[i][j].setForeground(Color.red);
-							board[i][j] = Integer.parseInt(textfields[i][j].getText().strip());
-						}
-					}
-				}
-
-			}
-		});
-		btnSolve.addActionListener(e-> solve());
-	}
-	
-	private void setNum() {
-		num = new int[9];
-		for(int i = 0; i < num.length; i++) 
-			num[i] = i+1;
-		board = new int[9][9];
-	}
-	
-	int[][][] map = new int[9][9][9];
-	
-	private void solve() {
-		check();
-	}
-	
-	private void check() {
-		checkcolumn();
+//package algorithm;
+//
+//import java.awt.BorderLayout;
+//import java.awt.Color;
+//import java.awt.Font;
+//import java.awt.GridLayout;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.util.Arrays;
+//import java.util.Collections;
+//
+//import javax.swing.JButton;
+//import javax.swing.JFrame;
+//import javax.swing.JPanel;
+//import javax.swing.JTextField;
+//import javax.swing.event.DocumentEvent;
+//import javax.swing.event.DocumentListener;
+//
+//
+//public class SudokuSolver {
+//	public static void main(String[] args) {
+//		Solver solver = new Solver();
+//		
+//	}
+//}
+//
+//class Solver extends JFrame{
+//	JPanel panel;
+//	JPanel panel1;
+//	JButton btnSet;
+//	JButton btnSolve;
+//	JButton btnReset;
+//	JTextField[][] textfields;
+//	int[][] board;
+//	int[] nums;
+//	
+//	Solver() {
+//		
+//		setNum();
+//		initBoard();
+//		this.pack();
+//		this.setSize(420,420);
+//		this.setLocationRelativeTo(null);
+//		this.setVisible(true);
+//		setBtns();
+//	}
+//	
+//	private void initBoard () {
+//		panel = new JPanel();
+//		panel.setLayout(new GridLayout(9, 9));
+//		textfields = new JTextField[9][9]; 
+//		for(int i = 0; i < 9; i++) {
+//			for(int j = 0; j < 9; j++) {
+//				textfields[i][j] = new JTextField();
+//				textfields[i][j].setFont(new Font("Tahoma", Font.BOLD, 17));
+//				textfields[i][j].setHorizontalAlignment(JTextField.CENTER);
+////				textfields[i][j].setText(String.valueOf(i+j*9));
+//				panel.add(textfields[i][j]);
+//				textfields[i][j].getDocument().addDocumentListener(new Doclistener(i,j));
+//			}
+//		}
+//		getContentPane().add(panel, BorderLayout.CENTER);
+//		panel1 = new JPanel();
+//		btnSet = new JButton("Set");
+//		panel1.add(btnSet);
+//		btnSolve = new JButton("Solve");
+//		panel1.add(btnSolve);
+//		btnReset = new JButton("Reset");
+//		panel1.add(btnReset);
+//		getContentPane().add(panel1, BorderLayout.SOUTH);
+//	}
+//	
+//	class Doclistener implements DocumentListener {
+//		int i;
+//		int j;
+//		Doclistener(int i, int j) {
+//			this.i = i;
+//			this.j = j;
+//		}
+//		@Override
+//		public void insertUpdate(DocumentEvent e) {
+//			update(e);
+//		}
+//		@Override
+//		public void removeUpdate(DocumentEvent e) {
+//			update(e);
+//		}
+//		@Override
+//		public void changedUpdate(DocumentEvent e) {
+//			update(e);
+//		}
+//		void update(DocumentEvent e) {
+//			if(e.getDocument().getLength()>0){
+//				if(j==8) {
+//					if(i==8) return;
+//					textfields[i+1][0].requestFocus();
+//					return;
+//				} 
+//				textfields[i][j+1].requestFocus();
+//			}
+//		}
+//	}
+//	
+//	private void setBtns() {
+//		btnSet.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				for(int i = 0; i < 9; i++) {
+//					for(int j = 0; j < 9; j++) {
+//						if(!textfields[i][j].getText().isBlank()) {
+//							textfields[i][j].setForeground(Color.red);
+//							board[i][j] = Integer.parseInt(textfields[i][j].getText().strip());
+//						}
+//					}
+//				}
+//
+//			}
+//		});
+//		btnSolve.addActionListener(e-> solve());
+//	}
+//	
+//	private void setNum() {
+//		nums = new int[9];
+//		for(int i = 0; i < nums.length; i++) 
+//			nums[i] = i+1;
+//		board = new int[9][9];
+//	}
+//	//map[][][] and num[] length should be 10 to support 1-9 and 0 as indication of no data
+//	int[][][] map = new int[9][9][10];
+//	
+//	private void solve() {
+//		check();
+//	}
+//	
+//	private void test() {
+//		//number of possibilities
+//		int index = 9;
+//		int a = 0;
+//		int b = 0;
+//		for(int i = 0; i < 9; i++) {
+//			for(int j = 0; j < 9; j++) {
+//				reverseSort(map[i][j]);
+//				if(Arrays.binarySearch(map[i][j], 0)<index) {
+//					index = Arrays.binarySearch(map[i][j], 0);
+//					a = i;
+//					b = j;
+//				}
+//			}
+//		}
+//		
+//		
+//	}
+//	
+//	private void check() {
+//		//column should be first since this method does not check for 0
+//		checkcolumn();
 //		checkrow();
-		for(int i =0; i < 9; i++) {
-			for(int j = 0; j < 9; j++) {
-				System.out.println(Arrays.toString(map[i][j]));
-			}
-		}
-	}
-	private void checkbox() {
-		
-	}
-	//TODO try using stream to add array to array
-	private void checkrow() {
-		for(int i = 0; i < 9; i++) {
-			int[] num = new int[9];
-			for(int j = 0; j < 9; j++) {
-				num[j] = board[j][i];	
-			}
-			for(int k = 0; k < 9; k++) {
-				if(num[k] != 0) continue;
-				for(int g = 0; g < 9; g++) {
-					if(map[i][k][g] != 0) continue;
-					map[i][k][g] = num[k];
-				}
-			}
-		}
-	}
-	private void checkcolumn() {
-		for(int i = 0; i < 9; i++) {
-			int[] num = new int[9];
-			for(int j = 0; j < 9; j++) {
-				num[j] = board[i][j];	
-			}
-			for(int k = 0; k < 9; k++) {
-				map[i][k] = num;
-			}
-		}
-	}
-	
-}
-
-
-
-
-
-
-
-
-
+//		checkbox();
+//		for(int i =0; i < 9; i++) {
+//			for(int j = 0; j < 9; j++) {
+//				System.out.println(Arrays.toString(map[i][j]));
+//			}
+//		}
+//	}
+//	private void checkbox() {
+//
+//		for(int k = 0; k < 3; k++) {
+//			for(int h = 0; h < 3; h++) {
+//				int num[] = new int[10];
+//				for(int i = 0; i < 3; i++) {
+//					for(int j = 0; j < 3; j++) {
+//						num[board[i+k*3][j+h*3]] = board[i+k*3][j+h*3];
+//					}
+//				}
+//				for(int i = 0; i < 3; i++) {
+//					for(int j = 0; j < 3; j++) {
+//						for(int g = 0; g < 10; g++) {
+//							if(map[i+k*3][j+h*3][g] == 0 ) {
+//								map[i+k*3][j+h*3][g] = num[g];
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	//TODO try using stream to add array to array
+//	private void checkrow() {
+//		for(int i = 0; i < 9; i++) {
+//			int[] num = new int[10];
+//			for(int j = 0; j < 9; j++) {
+//				num[board[j][i]] = board[j][i];	
+//			}
+//			//Arrays.sort(num, Collections.reverseOrder()); does not work because array is of primitive type
+//			//manually sorting array to descending order
+//			for(int k = 0; k < 9; k++) {
+//				//can't do map[k][i] = num because num reference will be copied 
+//				for(int g = 0; g < 10; g++) {
+//					if(map[k][i][g] == 0 ) {
+//						map[k][i][g] = num[g];
+//					}
+//				}
+//			}
+//		}
+//	}
+//	private void checkcolumn() {
+//		for(int i = 0; i < 9; i++) {
+//			int[] num = new int[10];
+//			for(int j = 0; j < 9; j++) {
+//				num[board[i][j]] = board[i][j];	
+//			}
+//			for(int k = 0; k < 9; k++) {
+//				for(int g = 0; g < 10; g++) {
+//					map[i][k][g] = num[g];
+//				}
+//			}
+//		}
+//	}
+//	
+//	
+//	private void reverseSort(int[] array) {
+//		for(int i = 0; i < array.length-1; i++) {
+//			for(int j = 0; j < array.length-1; j++) {
+//				if(array[j] < array[j+1]) {
+//					int a = array[j+1];
+//					array[j+1] = array[j];
+//					array[j] = a;
+//				}
+//			}
+//		}
+//	}
+//	
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
---
+++ javademo/src/algorithm/Test.java
@@ -0,0 +1,137 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class Test {
+	public static void main(String[] args) {
+		T t = new T();
+		
+	}
+}
+class T {
+	int[][] board = {
+			{0,0,0,0,0,5,1,0,0},
+			{0,7,4,0,0,0,0,2,0},
+			{0,0,9,0,0,0,0,0,3},
+			{0,0,3,0,0,0,0,0,0},
+			{7,6,0,0,8,0,0,0,0},
+			{4,0,0,6,0,1,0,0,0},
+			{0,0,6,2,4,0,3,0,9},
+			{0,0,0,7,6,3,0,0,2},
+			{0,0,0,0,0,0,0,4,0}
+	};
+	int[][] original;
+	T(){
+		setOriginal();
+		solve(1,0,0);
+		System.out.println(original[4][1]);
+		
+		System.out.println("Finished");
+		for(int i = 0; i < 9; i++) {
+			System.out.println(Arrays.toString(board[i]));
+		}
+
+	}
+	boolean solved = false;
+	private void solve(int num, int row, int column) {	
+		
+		//if no num fits(>9) return one recursion
+		if(num > 9) {
+			return;
+		}
+		if(column > 8) {
+			row += 1;
+			column = 0;
+			num = 1;
+		}
+		if(row > 8) {
+			System.out.println("solved");
+			solved = true;
+			return;
+		}
+	
+		
+		//if selected cell is original, skip
+		while(true) {
+			if(row>8) {
+				solved = true;
+				System.out.println(solved+"ROW"+row+"COLUMN"+column);
+				break;
+			}
+			if(original[row][column] == 0) break;
+			if(column == 8) {
+				row += 1;
+				column = 0;
+			} else {
+				column += 1;
+			}
+			num = 1;
+		}
+
+
+		
+		if(row == 4&&column==1) System.out.println("WHY");	
+		//check for given number
+		if(check(num,row,column)) {
+			board[row][column] = num;
+			solve(1, row, column+1);
+			//if no number can fit, go back one recursion and try higher num, if that does not work go back one more
+			if(solved) {
+				return;
+			}
+		} 
+		solve(num+1,row,column);
+		 
+		//remove cell when exiting this recursion
+		if(solved){
+			return;
+		} else {
+			board[row][column] = 0;
+		}
+	}
+	
+	
+	private boolean check(int num, int row, int column) {
+		return checkrow(num, row)&&checkcolumn(num, column)&&checkbox(num, row, column);
+			
+	}
+	private boolean checkcolumn(int num, int column) {
+		for(int i = 0; i < 9; i++) {
+			if(board[i][column] == num)
+				return false;
+		}
+		return true;
+	}
+	
+	private boolean checkrow(int num, int row) {
+		for(int i = 0; i < 9; i++) {
+			if(board[row][i] == num)
+				return false;
+		}
+		return true;
+	}
+	
+	private boolean checkbox(int num, int row, int column) {
+		int a = row%3;
+		int b = column%3;
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				if(board[row-a+i][column-b+j] == num)
+					return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public void setOriginal() {
+	
+		original = new int[9][9];
+		for(int i = 0; i< 9;i++) {
+			for(int j = 0; j<9;j++) {
+				original[i][j] = board[i][j];
+			}
+		}
+		
+	}
+}
