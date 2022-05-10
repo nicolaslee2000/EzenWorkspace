c095c271ff5d2b1c2074edaf5d048a764f6cf446
nicolaslee2000 <nicolaslee2000@gmail.com>
2022-05-11 08:11:27 +0900

    update

--- javademo/src/test02/Test01.java
+++ javademo/src/test02/Test01.java
@@ -1,11 +1,18 @@
 package test02;
 
-import lombok.Data;
-import lombok.Getter;
-
 public class Test01 {
 	public static void main(String[] args) {
-		
+		new vararg().takeInput(1,"asdf",3.3);
 	}
 }
 
+class vararg {
+	vararg() {
+		
+	}
+	public void takeInput(Object... obj) {
+		for(Object o : obj) {
+			System.out.println(o.getClass().getTypeName());
+		}
+	}
+}
